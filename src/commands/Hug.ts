c3627fe84bff6ae9faf4b665199d2cc1bf3810a7
Dylan Ogle <delxhq@funo.network>
2019-09-15 18:10:13 +0100

    Hug command.

---
+++ src/commands/Hug.ts
@@ -0,0 +1,23 @@
+import { Message } from 'discord.js'
+
+import { Category, Command } from '../Command'
+import { Funo } from '../Funo'
+import { Image } from '../utils'
+
+import client from 'nekos.life'
+
+export const Hug = new (class extends Command {
+
+  public name = 'hug'
+  public category = Category.Fun
+  public description = 'Hug a user in a Discord server.'
+  public aliases = []
+
+  public async run(funo: Funo, msg: Message, args: string[]) {
+    const member = msg.guild.member(msg.mentions.users.first() || args[0])
+
+    new client().sfw.hug().then(imageBody => {
+      msg.channel.send(Image(imageBody.url, `${msg.author} hugs ${member}`))
+    })
+  }
+})
